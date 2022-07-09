c3b09a4abe70e02f42bc68d023fd13bbba17bde0
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2022-07-09 19:48:16 +0800

    edits

--- src/Components/ProfileForm.js
+++ src/Components/ProfileForm.js
@@ -3,17 +3,19 @@ import classes from "./ProfileForm.module.css";
 
 function ProfileForm() {
   return (
-    <form className={classes.signup}>
-      <input placeholder="Name"></input>
-      <input placeholder="Email"></input>
-      <input placeholder="Password"></input>
-      <input placeholder="Confirm password"></input>
-      {/* <input
+    <div className={classes.root}>
+      <form className={classes.signup}>
+        <input placeholder="Name"></input>
+        <input placeholder="Email"></input>
+        <input placeholder="Password"></input>
+        <input placeholder="Confirm password"></input>
+        {/* <input
         className={classes.submit}
         type="submit"
         value="Create Account"
       ></input> */}
-    </form>
+      </form>
+    </div>
   );
 }
 
