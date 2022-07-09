cf8cef13c8c8b9de4cf8e57edb26c895b08d66db
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2022-07-09 15:29:09 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import React from "react";
+
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/landing" element={<LandingPage />} />
+        <Route exact path="/students" />
+        <Route exact path="/funds" />
+        <Route exact path="/profile" />
+        <Route exact path="/registration" />
+        <Route path="/*" element={<Navigate to="/landing" />} />
+      </Routes>
+    </Router>
   );
 }
 
