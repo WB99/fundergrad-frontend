21dd44345b789efd6ee64ccc879bba705c640b96
marctan2705 <marctan2705@gmail.com>
2022-07-09 16:34:52 +0800

    landing page done

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -6,7 +6,17 @@ function LandingPage() {
   return (
     <div className="landing-page">
       <NavBar />
-      <div className="landing-page-holder">hlwqdjk</div>
+      <div className="landing-page-holder">
+        <div className="quote"> Empowering Students</div>
+        <div className="button-holder-landing">
+          <button className="landing-button">
+            Log In
+          </button>
+          <button className="landing-button signup">
+            Sign up
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
