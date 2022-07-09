a3e7de40b8e7f29c5f6efdf56fd6d7b2afd273f1
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2022-07-09 21:06:48 +0800

    edits

--- src/Components/ProfileForm.js
+++ src/Components/ProfileForm.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./ProfileForm.module.css";
 
 function ProfileForm() {
   return (
     <div className={classes.root}>
       <form className={classes.signup}>
-        <input placeholder="Name"></input>
-        <input placeholder="Email"></input>
-        <input placeholder="Password"></input>
-        <input placeholder="Confirm password"></input>
-        {/* <input
-        className={classes.submit}
-        type="submit"
-        value="Create Account"
-      ></input> */}
+        <text>School :</text>
+        <input className={classes.input} />
+        <text>Degree :</text>
+        <input className={classes.input} />
+        <text>Year of Study :</text>
+        <input className={classes.input} />
+        <text>LinkedIn :</text>
+        <input className={classes.input} />
+        <input
+          className={classes.submit}
+          type="submit"
+          value="Update Profile"
+        ></input>
       </form>
     </div>
   );
