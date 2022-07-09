e80484449d472916d377cf96bc30bdccf07c0bca
marctan2705 <marctan2705@gmail.com>
2022-07-09 19:47:52 +0800

    login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Login.css"
-function Login() {
+function Login({changePage}) {
     return ( 
         <form className="signup">
             <div className="create-account login">Log In</div>
             <input placeholder='Email'></input>
             <input placeholder='Password'></input>
             <input className="submit" type="submit" value="Create Account"></input>
+            <div>Don't have an account?</div>
+            <div className="changepage" onClick={changePage}>Sign up here!</div>
         </form>
      );
 }
