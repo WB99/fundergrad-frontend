bcc99636d56977dbe0b103ccd9109ae9cf4e60ac
Daniel Yang <danielyang@Daniels-MacBook-Pro.local>
2022-07-10 11:50:31 +0800

    edits

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ function App() {
       degree: "Computer Science",
       year: 1,
       LI: "linkedin.com/chukang",
-      profilepic: joel,
+      profilePicUri: joel,
       resume:
         "https://drive.google.com/file/d/1zUpmTruNMvEH03cLHr9pN1VWG1JxLXP_/view?usp=sharing",
       role: "student",
@@ -143,7 +143,7 @@ function App() {
       degree: "Business",
       year: "Graduated",
       LI: "linkedin.com/meng",
-      profilepic: joel,
+      profilePicUri: joel,
       resume:
         "https://drive.google.com/file/d/1zUpmTruNMvEH03cLHr9pN1VWG1JxLXP_/view?usp=sharing",
       role: "donor",
