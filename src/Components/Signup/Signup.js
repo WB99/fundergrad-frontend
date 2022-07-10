bc2c7233b12a16bfacec26c40e4fa4ae8322d92d
matthew <matthew@secrettune.io>
2022-07-10 09:40:48 +0800

    added password stuff

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,74 +1,81 @@
-import React, { useEffect, useState, useContext, useDebugValue} from 'react'
-import { SignupContext, ProfilesContext } from '../../App'
-import { useNavigate } from 'react-router-dom'
-import "./Signup.css"
-function Signup({changePage}) {
-    const navigate = useNavigate()
-    let userDetails = useContext(SignupContext)
-    let profiles = useContext(ProfilesContext)
-    const [formFilled, setFormFilled] = useState(false)
-    const [role, setRole] = useState("student")
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordConfirm, setPasswordConfirm] = useState("")
-    const [match, setMatch] = useState(true)
-    const [submitCount, setSubmitCount] = useState(0)
-    function matchHandle(){
-        setMatch(!((passwordConfirm.length >= 1) && passwordConfirm != password))
-        console.log(passwordConfirm.length)
-    }
-    function handleName(event){
-        setName(event.target.value);
-        console.log(name)
-    }
-    function handleEmail(event){
-        setEmail(event.target.value);
-        console.log(name)
-    } 
-    function handlePassword(event){
-        setPassword(event.target.value);
-        console.log(name)
-    }
-    function handlePasswordConfirm(event){
-        setPasswordConfirm(event.target.value);
-        // matchHandle();
-        console.log(name)
-    }
-    function setSignupContext(e){
-        e.preventDefault()
-        userDetails = {name: name, email: email, password: password, role: role}
-        profiles.push(userDetails)
-        navigate("/profile")
-    }
+import React, { useEffect, useState, useContext, useDebugValue } from "react";
+import { SignupContext, ProfilesContext } from "../../App";
+import { useNavigate } from "react-router-dom";
+import "./Signup.css";
+function Signup({ changePage }) {
+  const navigate = useNavigate();
+  let userDetails = useContext(SignupContext);
+  let profiles = useContext(ProfilesContext);
+  const [formFilled, setFormFilled] = useState(false);
+  const [role, setRole] = useState("student");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [match, setMatch] = useState(true);
+  const [passwordError, setPasswordError] = useState(false);
+  const [touched, setTouched] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  function matchHandle() {
+    setMatch(!(passwordConfirm.length >= 1 && passwordConfirm != password));
+  }
+  function handleName(event) {
+    setName(event.target.value);
+  }
+  function handleEmail(event) {
+    setEmail(event.target.value);
+  }
+  function handlePassword(event) {
+    setTouched(true);
+    setPassword(event.target.value);
+  }
+  function handlePasswordConfirm(event) {
+    setPasswordConfirm(event.target.value);
+  }
+
+  const toggleShowPassword = () => {
+    setShowPassword(() => !showPassword);
+  };
 
   useEffect(() => {
     matchHandle();
     if (
-      name != "" &&
-      email != "" &&
+      name !== "" &&
+      email !== "" &&
       password.length > 8 &&
-      password == passwordConfirm
+      password === passwordConfirm
     ) {
       setFormFilled(true);
     } else {
       setFormFilled(false);
     }
   }, [passwordConfirm, name, email, password]);
-  // const [passwordError, setPasswordError] = useState(false)
-  // const passwordChecker = () => {
-  //   if (password.length > 9){
-  //     setPasswordError(false)
-  //   }
-  // }
-  const submitHandler = (e) => {
-    e.preventDefault()
-    const userInfo = JSON.stringify({ name: name, email: email, password: password, role: role });
-    // console.log(useri)
-    localStorage.setItem("user", userInfo)
-    navigate("/profile");
 
-  }
+  const passwordChecker = () => {
+    if (password.length <= 8 && touched) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+  };
+
+  useEffect(() => {
+    passwordChecker();
+  }, [password]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const userInfo = JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+      role: role,
+    });
+    localStorage.setItem("user", userInfo);
+    navigate("/profile");
+  };
+
   return (
     <form className="signup">
       <div className="create-account">Create an account</div>
@@ -93,34 +100,34 @@ function Signup({changePage}) {
       </div>
       <input placeholder="Name" onChange={handleName}></input>
       <input placeholder="Email" onChange={handleEmail}></input>
-      <input placeholder="Password" onChange={handlePassword}></input>
-      <input
-        placeholder="Confirm password"
-        onChange={handlePasswordConfirm}
-      ></input>
-      {match ? null : <div className="matchy">Passwords do not match</div>}
-      {/* {formFilled ? (
-        <button className="submit" onClick={setSignupContext}>
-          Create Account
-        </button>
-      ) : (
-        <button
-          className="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            setSubmitCount(submitCount + 1);
-          }}
-        >
-          Create Account
-        </button>
-      )} */}
-      <button className="submit" disabled={!formFilled} onClick={submitHandler} >Create Account</button>
-      {formFilled == false && submitCount > 0 ? (
-        <div className="matchy">
-          Please ensure a name, email and password of more than length 8 is
-          submitted.
+      <div className="inputContainer">
+        <input
+          placeholder="Password"
+          type={!showPassword ? "password" : "text"}
+          onChange={handlePassword}
+        />
+        <div className="eye" onClick={toggleShowPassword}>
+          👁
         </div>
-      ) : null}
+      </div>
+      {passwordError && (
+        <div className="matchy">
+          Password must be more than 8 characters long.
+        </div>
+      )}
+      <div className="inputContainer">
+        <input
+          placeholder="Confirm password"
+          onChange={handlePasswordConfirm}
+        />
+        <div className="eye" onClick={toggleShowPassword}>
+          👁
+        </div>
+      </div>
+      {match ? null : <div className="matchy">Passwords do not match</div>}
+      <button className="submit" disabled={!formFilled} onClick={submitHandler}>
+        Create Account
+      </button>
       <div>Have an account?</div>
       <div className="changepage" onClick={changePage}>
         Log in here!
